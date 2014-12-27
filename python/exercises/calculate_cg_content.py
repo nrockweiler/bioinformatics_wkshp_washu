acbbf4e13866f7a1bb3ac2a7fff4828e199ecd2c
Molly Gibson <mollykrisann@gmail.com>
2014-12-27 15:31:56 -0600

    update exercises for cg content

---
+++ python/exercises/calculate_cg_content.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python                                                                                                                                                                                                                        
+
+# python imports
+import sys
+ 
+# get the DNA string from STDIN
+dna_string = sys.stdin.readline()
+
+# calculate CG content of dna_string and assign to variable cg_content
+# TODO: Your code goes here
+
+
+
+
+
+# Print out the calculated cg content
+print cg_content
