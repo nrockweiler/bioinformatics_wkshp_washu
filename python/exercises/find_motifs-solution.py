c8f930a5cf17f1fb6f41cc01ce5f1936841b0e7d
Molly Gibson <molly.gibson@wustl.edu>
2014-12-31 16:07:01 -0600

    update motif exercises

--- python/exercises/find_motifs-solution.py
+++ python/exercises/find_motifs-solution.py
@@ -22,14 +22,14 @@ motif_locs = []
 # Iterate through the dna_string to find the motif occurrences
 for position, nucleotide in enumerate(dna_string):
     # position is the current position in the string, e.g., 5.  Note: the first position is 0 not 1!
-	# nucleotide is the character at the current position in the string, e.g., A.
+    # nucleotide is the character at the current position in the string, e.g., A.
 	
-	# Retrieve the subsequence from dna_string starting at the current position and is motif_len nucleotides long
+    # Retrieve the subsequence from dna_string starting at the current position and is motif_len nucleotides long
     seq_in_position = dna_string[position:position+motif_len]
 
-	# If the subsequence matches the motif sequence, add this position to the list
+    # If the subsequence matches the motif sequence, add this position to the list
     if seq_in_position == motif:
-        motif_locs.append(position+1) # The +1 is to make the position start from 0 instead of 1.
+        motif_locs.append(position+1) # The +1 is to make the position start from 1 instead of 0.
 
 # Print out the motif locations
 print(motif_locs)
